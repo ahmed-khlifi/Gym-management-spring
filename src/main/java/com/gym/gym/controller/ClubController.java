bbfc1646f8ddc66a9ceb794a722ffbd5e9b6177d
ahmed-khlifi <ahmedkh3lifi@gmail.com>
2024-12-16 20:50:29 +0100

    (feat) clubs: display clubs list

--- src/main/java/com/gym/gym/controller/ClubController.java
+++ src/main/java/com/gym/gym/controller/ClubController.java
@@ -1,18 +1,31 @@
 package com.gym.gym.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gym.gym.service.ClubService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/club")
 public class ClubController {
 
+    private ClubService clubService;
+
+    @Autowired
+    public ClubController(ClubService clubService) {
+        this.clubService = clubService;
+    }
+
     @GetMapping("/all")
-    public String getAllClub() {
-        return new String();
+    public String getAllClub(Model model) {
+        model.addAttribute("clubs", clubService.findAll());
+        return "club_list";
     }
 
     @GetMapping("/create")
